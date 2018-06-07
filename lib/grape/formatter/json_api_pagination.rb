519f66447cf5f1401c13af85f1132d03b4b6636d
Matt Gibson <matt@farmdrop.com>
2018-06-07 10:09:37 +0100

    Extra spec to keep track of existing functionality during refactor

--- lib/grape/formatter/json_api_pagination.rb
+++ lib/grape/formatter/json_api_pagination.rb
@@ -1,140 +1,157 @@
 module Grape
   module Formatter
-    module JsonApiPagination
+    class JsonApiPagination
       class << self
         def call(resource, env)
-          serialize_resource(resource, env) || Grape::Formatter::Json.call(resource, env)
+          new.serialize_resource(resource, env) || Grape::Formatter::Json.call(resource, env)
+        end
+      end
+
+      attr_reader :env, :resource
+
+      def serialize_resource(resource, env)
+        @env = env
+        @resource = resource
+
+        endpoint = env['api.endpoint']
+
+        jsonapi_options = build_options_from_endpoint(endpoint)
+        jsonapi_options.merge!(env['jsonapi_options'] || {})
+
+        context = {}
+        context[:current_user] = endpoint.current_user if endpoint.respond_to?(:current_user)
+        context.merge!(jsonapi_options.delete(:context)) if jsonapi_options[:context]
+
+        resource_class = resource_class_for(resource)
+
+        if resource_class.nil?
+          return blank_resource_response(jsonapi_options, resource)
         end
 
-        def serialize_resource(resource, env)
-          endpoint = env['api.endpoint']
-
-          jsonapi_options = build_options_from_endpoint(endpoint)
-          jsonapi_options.merge!(env['jsonapi_options'] || {})
-
-          context = {}
-          context[:current_user] = endpoint.current_user if endpoint.respond_to?(:current_user)
-          context.merge!(jsonapi_options.delete(:context)) if jsonapi_options[:context]
-
-          resource_class = resource_class_for(resource)
-
-          if resource_class.nil?
-            return blank_resource_response(jsonapi_options, resource)
+        sorted_primary_ids = nil
+        if resource.respond_to?(:to_ary)
+          resource_instances = resource.to_ary.compact.collect do |each_resource|
+            each_resource_class = resource_class_for(each_resource)
+            each_resource_class.new(each_resource, context)
           end
+          sorted_primary_ids = resource_instances.collect { |resource_instance| resource_instance.try(:id) }
+        else
+          resource_instances = resource_class.new(resource, context)
+        end
 
-          sorted_primary_ids = nil
-          if resource.respond_to?(:to_ary)
-            resource_instances = resource.to_ary.compact.collect do |each_resource|
-              each_resource_class = resource_class_for(each_resource)
-              each_resource_class.new(each_resource, context)
-            end
-            sorted_primary_ids = resource_instances.collect { |resource_instance| resource_instance.try(:id) }
-          else
-            resource_instances = resource_class.new(resource, context)
-          end
-
-          resource_serialized = JSONAPI::ResourceSerializer
+        resource_serialized = JSONAPI::ResourceSerializer
                                 .new(resource_class, jsonapi_options)
                                 .serialize_to_hash(resource_instances)
-          json_output = if jsonapi_options[:meta]
-                          # Add option to merge top level meta tag as
-                          # jsonapi-resources does not appear to support this
-                          resource_serialized.as_json.merge(meta: jsonapi_options[:meta])
-                        else
-                          resource_serialized
-                        end
+        json_output = if jsonapi_options[:meta]
+                        # Add option to merge top level meta tag as
+                        # jsonapi-resources does not appear to support this
+                        resource_serialized.as_json.merge(meta: jsonapi_options[:meta])
+                      else
+                        resource_serialized
+                      end
 
-          # Ensure sort order is maintained, serialize_to_hash can reorder objects if
-          # objects of the array are of different types (polymorphic cases)
-          json_output = json_output.stringify_keys
-          if sorted_primary_ids && (data = json_output["data"]).present?
-            sorted_primary_ids = sorted_primary_ids.map(&:to_s)
-            json_output["data"] = data.sort_by { |d| sorted_primary_ids.index(d["id"]) }
-          end
+        # Ensure sort order is maintained, serialize_to_hash can reorder objects if
+        # objects of the array are of different types (polymorphic cases)
+        json_output = json_output.stringify_keys
+        if sorted_primary_ids && (data = json_output["data"]).present?
+          sorted_primary_ids = sorted_primary_ids.map(&:to_s)
+          json_output["data"] = data.sort_by { |d| sorted_primary_ids.index(d["id"]) }
+        end
 
-          if resource.respond_to?(:page)
-            json_output["links"] ||= {}
+        if resource.respond_to?(:current_page) && resource.respond_to?(:total_pages)
+          json_output["links"] ||= {}
 
-            original_uri = URI("#{jsonapi_options[:base_url]}#{env['REQUEST_URI']}")
-            json_output["links"]["self"] = original_uri.to_s
+          original_uri = URI("#{jsonapi_options[:base_url]}#{env['REQUEST_URI']}")
+          json_output["links"]["self"] = original_uri.to_s
 
-            original_query = Rack::Utils.parse_query(original_uri.query)
+          original_query = Rack::Utils.parse_query(original_uri.query)
 
-            last_page_query = original_query.merge('page[number]' => resource.total_pages)
-            last_page_uri = original_uri.dup.tap { |uri| uri.query = build_query(last_page_query) }
-            json_output["links"]["last"] = last_page_uri.to_s
+          last_page_query = original_query.merge('page[number]' => resource.total_pages)
+          last_page_uri = original_uri.dup.tap { |uri| uri.query = build_query(last_page_query) }
+          json_output["links"]["last"] = last_page_uri.to_s
 
-            first_page_query = original_query.merge('page[number]' => 1)
-            first_page_uri = original_uri.dup.tap { |uri| uri.query = build_query(first_page_query) }
-            json_output["links"]["first"] = first_page_uri.to_s
+          first_page_query = original_query.merge('page[number]' => 1)
+          first_page_uri = original_uri.dup.tap { |uri| uri.query = build_query(first_page_query) }
+          json_output["links"]["first"] = first_page_uri.to_s
 
-            prev_page_query = original_query.merge('page[number]' => [1, resource.current_page.pred].max )
+          if resource.current_page > 1
+            prev_page_query = original_query.merge('page[number]' => [1, resource.current_page.pred].max)
             prev_page_uri = original_uri.dup.tap { |uri| uri.query = build_query(prev_page_query) }
             json_output["links"]["prev"] = prev_page_uri.to_s
+          end
 
+          if resource.current_page < resource.total_pages && resource.total_pages > 1
             next_page_query = original_query.merge('page[number]' => [resource.total_pages, resource.current_page.succ].min)
             next_page_uri = original_uri.dup.tap { |uri| uri.query = build_query(next_page_query) }
             json_output["links"]["next"] = next_page_uri.to_s
           end
-
-          json_output.to_json
         end
 
-        def build_query(params)
-          params.map { |k, v|
-            if v.class == Array
-              build_query(v.map { |x| [k, x] })
-            else
-              v.nil? ? k : "#{k}=#{v}"
-            end
-          }.join("&")
-        end
+        json_output.to_json
+      end
 
-        def build_options_from_endpoint(endpoint)
-          options = {}
-          if endpoint.namespace_inheritable(:jsonapi_base_url)
-            options[:base_url] = endpoint.namespace_inheritable(:jsonapi_base_url)
-          end
-          options
-        end
+      def original_uri(jsonapi_options)
+        URI("#{jsonapi_options[:base_url]}#{env['REQUEST_URI']}")
+      end
 
-        def resource_class_for(resource)
-          if resource.class.respond_to?(:jsonapi_resource_class)
-            resource.class.jsonapi_resource_class
-          elsif resource.respond_to?(:to_ary)
-            resource_class_for(resource.to_ary.first)
+      def uri_with_page_number(page_number)
+        original_query.merge('page[number]' => page_number)
+      end
+
+      def build_query(params)
+        params.map { |k, v|
+          if v.class == Array
+            build_query(v.map { |x| [k, x] })
           else
-            get_resource_for(resource.class)
+            v.nil? ? k : "#{k}=#{v}"
+          end
+        }.join("&")
+      end
+
+      def build_options_from_endpoint(endpoint)
+        options = {}
+        if endpoint.namespace_inheritable(:jsonapi_base_url)
+          options[:base_url] = endpoint.namespace_inheritable(:jsonapi_base_url)
+        end
+        options
+      end
+
+      def resource_class_for(resource)
+        if resource.class.respond_to?(:jsonapi_resource_class)
+          resource.class.jsonapi_resource_class
+        elsif resource.respond_to?(:to_ary)
+          resource_class_for(resource.to_ary.first)
+        else
+          get_resource_for(resource.class)
+        end
+      end
+
+      def resources_cache
+        @resources_cache ||= ThreadSafe::Cache.new
+      end
+
+      def get_resource_for(klass)
+        resources_cache.fetch_or_store(klass) do
+          resource_class_name = "#{klass.name}Resource"
+          resource_class = resource_class_name.safe_constantize
+
+          if resource_class
+            resource_class
+          elsif klass.superclass
+            get_resource_for(klass.superclass)
           end
         end
+      end
 
-        def resources_cache
-          @resources_cache ||= ThreadSafe::Cache.new
-        end
+      private
 
-        def get_resource_for(klass)
-          resources_cache.fetch_or_store(klass) do
-            resource_class_name = "#{klass.name}Resource"
-            resource_class = resource_class_name.safe_constantize
+      def blank_resource_response(jsonapi_options, resource)
+        return nil unless resource.blank?
 
-            if resource_class
-              resource_class
-            elsif klass.superclass
-              get_resource_for(klass.superclass)
-            end
-          end
-        end
-
-        private
-
-        def blank_resource_response(jsonapi_options, resource)
-          return nil unless resource.blank?
-
-          blank_return = {}
-          blank_return[:data] = resource.respond_to?(:to_ary) ? [] : {}
-          blank_return[:meta] = jsonapi_options[:meta] if jsonapi_options[:meta]
-          blank_return.to_json
-        end
+        blank_return = {}
+        blank_return[:data] = resource.respond_to?(:to_ary) ? [] : {}
+        blank_return[:meta] = jsonapi_options[:meta] if jsonapi_options[:meta]
+        blank_return.to_json
       end
     end
   end
--- spec/grape/formatter/json_api_pagination_spec.rb
+++ spec/grape/formatter/json_api_pagination_spec.rb
@@ -4,110 +4,190 @@ require 'spec_helper'
 require 'kaminari'
 
 Merchant = Struct.new(:id, :name)
+Product = Struct.new(:id, :name)
+
+class ProductResource < JSONAPI::Resource
+  attribute :name
+
+  has_one :merchant
+end
 
 class MerchantResource < JSONAPI::Resource
   attribute :name
+
+  has_one :product
 end
 
 RSpec.describe Grape::Formatter::JsonApiPagination do
   let(:base_url) { 'http://localhost:3000' }
+  let(:fake_endpoint) { double('endpoint', namespace_inheritable: base_url) }
+  let(:fake_env) do
+    {
+      'api.endpoint' => fake_endpoint
+    }
+  end
+  let(:resource) do
+    [
+      Merchant.new(12, 'Purton'),
+      Merchant.new(13, 'Anspach')
+    ]
+  end
 
-  context 'when there is no pagination supplied' do
-    it 'provides no pagination links' do
-      resource = [Merchant.new(12, 'Purton')]
-      fake_env = {
-        'api.endpoint' => double('endpoint', namespace_inheritable: base_url)
-      }
-      actual = described_class.call(resource, fake_env)
+  describe 'characterisation spec for the JSONAPI response' do
+    it 'matches the spec' do
       expected = {
-        data: [
+        'data' => [
           {
-            id: '12',
-            type: 'merchants',
-            links: {
-              self: 'http://localhost:3000/merchants/12'
+            'id' => '12',
+            'type' => 'merchants',
+            'links' => {
+              'self' => 'http://localhost:3000/merchants/12'
             },
-            attributes: {
-              name: 'Purton'
+            'attributes' => {
+              'name' => 'Purton'
+            },
+            'relationships' => {
+              'product' => {
+                'links' => {
+                  'self' => 'http://localhost:3000/merchants/12/relationships/product',
+                  'related' => 'http://localhost:3000/merchants/12/product'
+                }
+              }
+            }
+          },
+          {
+            'id' => '13',
+            'type' => 'merchants',
+            'links' => {
+              'self' => 'http://localhost:3000/merchants/13'
+            },
+            'attributes' => {
+              'name' => 'Anspach'
+            },
+            'relationships' => {
+              'product' => {
+                'links' => {
+                  'self' => 'http://localhost:3000/merchants/13/relationships/product',
+                  'related' => 'http://localhost:3000/merchants/13/product'
+                }
+              }
             }
           }
         ]
       }
-
-      expect(JSON.parse(actual)).to eq expected.as_json
+      json = described_class.call(resource, fake_env)
+      expect(JSON.parse(json)).to eq expected
     end
   end
 
-  context 'with a paginated resource supplied' do
-    let(:pagination_links) { JSON.parse(described_class.call(resource, fake_env))["links"] }
+  describe 'pagination' do
+    context 'when there is no pagination supplied' do
+      it 'provides no pagination links' do
+        resource = [Merchant.new(12, 'Purton')]
 
-    context 'when there is only one page' do
-      let(:fake_env) do
-        {
-          'api.endpoint' => double('endpoint', namespace_inheritable: base_url),
-          'REQUEST_URI' => '/merchants?page[number]=1&page[size]=2',
-          'PATH_INFO' => '/merchants',
-          'QUERY_STRING' => 'page[number]=1&page[size]=2'
-        }
-      end
-      let(:resource) do
-        Kaminari.paginate_array(
-          [
-            Merchant.new(12, 'Purton')
-          ]
-        ).page(1).per(2)
-      end
-
-      it 'provides pagination links' do
+        actual = described_class.call(resource, fake_env)
         expected = {
-          self: 'http://localhost:3000/merchants?page[number]=1&page[size]=2',
-          first: 'http://localhost:3000/merchants?page[number]=1&page[size]=2',
-          last: 'http://localhost:3000/merchants?page[number]=1&page[size]=2'
+          data: [
+            {
+              id: '12',
+              type: 'merchants',
+              links: {
+                self: 'http://localhost:3000/merchants/12'
+              },
+              attributes: {
+                name: 'Purton'
+              },
+              relationships: {
+                product: {
+                  links: {
+                    self: 'http://localhost:3000/merchants/12/relationships/product',
+                    related: 'http://localhost:3000/merchants/12/product'
+                  }
+                }
+              }
+            }
+          ]
         }
 
-        expect(pagination_links).to eq expected.as_json
+        expect(JSON.parse(actual)).to eq expected.as_json
       end
     end
 
-    context 'when there are multiple pages' do
-      context 'when we are on the middle page' do
+    context 'with a paginated resource supplied' do
+      let(:pagination_links) do
+        JSON.parse(described_class.call(resource, fake_env))['links']
+      end
+
+      context 'when there is only one page' do
         let(:fake_env) do
           {
-            'api.endpoint' => double('endpoint', namespace_inheritable: base_url),
-            'REQUEST_URI' => '/merchants?page[number]=2&page[size]=2',
+            'api.endpoint' => fake_endpoint,
+            'REQUEST_URI' => '/merchants?page[number]=1&page[size]=2',
             'PATH_INFO' => '/merchants',
-            'QUERY_STRING' => 'page[number]=2&page[size]=2'
+            'QUERY_STRING' => 'page[number]=1&page[size]=2'
           }
         end
         let(:resource) do
           Kaminari.paginate_array(
             [
-              Merchant.new(12, 'Purton'),
-              Merchant.new(13, 'Anspach'),
-              Merchant.new(14, 'Fourpure'),
-              Merchant.new(15, 'Naty'),
-              Merchant.new(16, 'Ranas'),
-              Merchant.new(17, 'Longmans'),
+              Merchant.new(12, 'Purton')
             ]
-          ).page(2).per(2)
+          ).page(1).per(2)
         end
 
-        it 'provides a pagination link for the first page' do
-          expect(pagination_links["first"]).to eq 'http://localhost:3000/merchants?page[number]=1&page[size]=2'
-        end
+        it 'provides pagination links' do
+          expected = {
+            self: 'http://localhost:3000/merchants?page[number]=1&page[size]=2',
+            first: 'http://localhost:3000/merchants?page[number]=1&page[size]=2',
+            last: 'http://localhost:3000/merchants?page[number]=1&page[size]=2'
+          }
 
-        it 'provides a pagination link for the last page' do
-          link = pagination_links["last"]
-          expect(link).to eq 'http://localhost:3000/merchants?page[number]=3&page[size]=2'
+          expect(pagination_links).to eq expected.as_json
         end
+      end
 
-        it 'provides a pagination link for the previous page' do
-          expect(pagination_links["prev"]).to eq 'http://localhost:3000/merchants?page[number]=1&page[size]=2'
-        end
+      context 'when there are multiple pages' do
+        context 'when we are on the middle page' do
+          let(:fake_env) do
+            {
+              'api.endpoint' => fake_endpoint,
+              'REQUEST_URI' => '/merchants?page[number]=2&page[size]=2',
+              'PATH_INFO' => '/merchants',
+              'QUERY_STRING' => 'page[number]=2&page[size]=2'
+            }
+          end
+          let(:resource) do
+            Kaminari.paginate_array(
+              [
+                Merchant.new(12, 'Purton'),
+                Merchant.new(13, 'Anspach'),
+                Merchant.new(14, 'Fourpure'),
+                Merchant.new(15, 'Naty'),
+                Merchant.new(16, 'Ranas'),
+                Merchant.new(17, 'Longmans'),
+              ]
+            ).page(2).per(2)
+          end
 
-        it 'provides a pagination link for the next page' do
-          link = pagination_links["next"]
-          expect(link).to eq 'http://localhost:3000/merchants?page[number]=3&page[size]=2'
+          it 'provides a pagination link for the first page' do
+            expected_link = 'http://localhost:3000/merchants?page[number]=1&page[size]=2'
+            expect(pagination_links['first']).to eq expected_link
+          end
+
+          it 'provides a pagination link for the last page' do
+            expected_link = 'http://localhost:3000/merchants?page[number]=3&page[size]=2'
+            expect(pagination_links['last']).to eq expected_link
+          end
+
+          it 'provides a pagination link for the previous page' do
+            expected_link = 'http://localhost:3000/merchants?page[number]=1&page[size]=2'
+            expect(pagination_links['prev']).to eq expected_link
+          end
+
+          it 'provides a pagination link for the next page' do
+            expected_link = 'http://localhost:3000/merchants?page[number]=3&page[size]=2'
+            expect(pagination_links['next']).to eq expected_link
+          end
         end
       end
     end
